9b1322e0ffeb2e5ba32678f0a46fab64743cb499
anjali7528 <anjaligandhi248@gmail.com>
2022-04-07 14:40:18 +0530

    UI Setup

---
+++ src/AddTransaction.js
@@ -0,0 +1,25 @@
+import React, { useState } from 'react'
+
+function AddTransaction() {
+    const [text, setText] = useState(" ");
+    const [amount, setAmount] = useState(0);
+
+  return (
+   <>
+   <h3>Add new Transaction</h3>
+   <form >
+       <div className="form-control">
+           <label htmlFor="text">Text</label>
+           <input type="text" value={text} onChange={(e) => setText(e.target.value)} placeholder="Enter Text ... "/>
+       </div>
+       <div className="form-control">
+           <label htmlFor="amount">Amount <br/>
+           (negative - expense, positive - income)</label>       
+        <input type="number"  value={amount} onChange={(e) => setAmount(e.target.value)} placeholder="Enter amount ... "/>
+       </div>
+   </form>
+   </>
+  )
+}
+
+export default AddTransaction
--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
+import Header from "./Header";
+import Balance from "./Balance";
+import IncomeExpenses from "./IncomeExpenses";
+import TransactionList from "./TransactionList";
+import AddTransaction from "./AddTransaction";
+
 
 function App() {
   return (
-    <div className="App">
-     <h1>Expense - Tracker</h1>
+  <div className="App">
+       <Header />
+    <div className="container">
+       <Balance />
+      <IncomeExpenses/>
+      <TransactionList/>
+      <AddTransaction/>
     </div>
+  </div>
   );
 }
 
